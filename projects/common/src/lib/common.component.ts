72d6a62d2ee528bbc9ad3a23b6600db11917dab6
Luc Fonjallaz <luc.fonjallaz@semabit.ch>
2019-02-09 21:04:00 +0100

    adding common

---
+++ projects/common/src/lib/common.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'brok-common',
+  template: `
+    <p>
+      common works!
+    </p>
+  `,
+  styles: []
+})
+export class CommonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
